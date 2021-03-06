995d1974a3d8943194ead1fd479e946b766b73e9
Louis Iaeger <louis.iaeger@gmail.com>
2021-03-06 10:52:03 -0600

    Make tour components more responsive for mobile (add breakpoints)

--- src/App.js
+++ src/App.js
@@ -65,20 +65,22 @@ const Navbar = () => {
 const About = () => {
   return (
     <>
-      <header className='title'>
-        <h2>About Us</h2>
-        <div className='underline'></div>
-      </header>
-      <p>
-        We've been delivering world class tours around the world for over 50
-        years. Whatever quinoa cardigan chia chillwave. Chartreuse cronut
-        gochujang godard prism trust fund. Jianbing kitsch subway tile VHS,
-        quinoa everyday carry keytar tilde cred semiotics master cleanse chia
-        taiyaki edison bulb. Etsy echo park listicle, normcore four loko
-        flexitarian pork belly scenester selfies hell of pok pok distillery blog
-        paleo. Art party pug craft beer food truck, cornhole listicle drinking
-        vinegar normcore roof party coloring book put a bird on it.
-      </p>
+      <div className='container-md'>
+        <header className='title'>
+          <h2>About Us</h2>
+          <div className='underline'></div>
+        </header>
+        <p>
+          We've been delivering world class tours around the world for over 50
+          years. Whatever quinoa cardigan chia chillwave. Chartreuse cronut
+          gochujang godard prism trust fund. Jianbing kitsch subway tile VHS,
+          quinoa everyday carry keytar tilde cred semiotics master cleanse chia
+          taiyaki edison bulb. Etsy echo park listicle, normcore four loko
+          flexitarian pork belly scenester selfies hell of pok pok distillery
+          blog paleo. Art party pug craft beer food truck, cornhole listicle
+          drinking vinegar normcore roof party coloring book put a bird on it.
+        </p>
+      </div>
     </>
   )
 }
